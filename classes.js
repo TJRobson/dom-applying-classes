248a136a379f9743b0c63ea07d0c54ae099da737
Dev <thomas.j.robson@hotmail.com>
2016-07-29 17:30:22 +1200

    changed div class invisible to be visible

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
  one()
  two()
  three()
+ makeVisible()
 
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -34,4 +35,10 @@ function three () {
   three.classList.add('red')
 }
 
-// CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+
+  var makeVisible = document.getElementsByClassName('invisible')
+
+  makeVisible[0].classList.add('visible')
+
+}
